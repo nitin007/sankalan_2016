e6b62dd4ca6048c214e2884a9f076e2324dbbbb3
Nitin Gupta <nitinguptawebdev@gmail.com>
2016-01-30 13:49:49 +0530

    Setup project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sankalan2016::Application.config.secret_token = '1eb4d05b1acc53a56492563275e6564cae351e0b6f277d961f97961582f0de1f2bc76923fe377bac3059ab71855a69eb3b3be0023198917ba3e0fb4c77cfa775'
